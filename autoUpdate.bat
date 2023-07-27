e94359b3ab25fc21a57f5a8f48861b8d27097956
“iiastudio” <chifu.research@gmail.com>
2023-07-27 19:42:56 +0800

    Add backup files

--- autoUpdate.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "E:\xampp\htdocs\iia_site\"
-git init
-git add .
-git commit -m "Add backup files"
-git remote add origin https://github.com/iiastudio/ea4_site
-git push -u origin master
