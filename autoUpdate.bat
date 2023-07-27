7a28309f6ce597887913b34e5d84356c2272fa6f
“iiastudio” <chifu.research@gmail.com>
2023-07-27 19:35:57 +0800

    Add backup files

--- autoUpdate.bat
+++ autoUpdate.bat
@@ -1,7 +1,7 @@
 @echo off
-cd /d "E:\xampp\htdocs\iia_site"  REM 導航到包含要備份的檔案的資料夾，用實際的路徑取代 "path/to/your/folder"
+cd /d "E:\xampp\htdocs\iia_site"
 git init
 git add .
 git commit -m "Add backup files"
-git remote add origin https://github.com/iiastudio/ea4_site  REM 將 <repository_url> 替換為你的 GitHub repository 的 URL 或名稱
+git remote add origin https://github.com/iiastudio/ea4_site
 git push -u origin master
