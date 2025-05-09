347ee78802ba3ff2f914b717f1f7e847fed4e142
chifuresearch <chifu.research@gmail.com>
2025-05-09 20:48:16 +0800

    update files

--- update.bat
+++ update.bat
@@ -1,8 +1,5 @@
 @echo off
-cd /d "E:/xampp/htdocs/iia_site/"
-git init
-git pull origin master
+cd /d "D:/xampp/htdocs/iia_site/"
 git add .
 git commit -m "update files"
-git remote add origin https://github.com/iiastudio/ea4_site
 git push -u origin master
